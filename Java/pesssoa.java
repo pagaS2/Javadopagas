5e545d0567b80d2591a41d4757306a4775baefa6
Gabriel Chaves Paganotto <gabrielpaganotto@gmail.com>
2022-11-09 18:51:41 -0300

    JAVA

---
+++ Java/pesssoa.java
@@ -0,0 +1,27 @@
+import java.util.Scanner; 
+ 
+class pessoa { 
+    String nome; 
+    float peso, altura, imc; 
+} 
+ 
+public class imc { 
+    public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in); 
+        pessoa p1; 
+        p1 = new pessoa(); 
+ 
+        System.out.println("Escreva seu nome completo:"); 
+        p1.nome = sc.next(); 
+        System.out.println("Escreva seu peso: "); 
+        p1.peso = sc.nextFloat(); 
+        System.out.println("Escreva sua altura: "); 
+        p1.altura = sc.nextFloat(); 
+ 
+        p1.imc = p1.peso / (p1.altura * p1.altura); 
+ 
+        System.out.println(p1.nome + " Seu imc é: " + "\t" + p1.imc); 
+ 
+        sc.close(); 
+    } 
+}
